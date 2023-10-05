d13b65b1d473f2d90e9332ea3e80ae15b1f3cdcb
kaulaskar <saileshkaulaskar@gmail.com>
2023-10-05 14:13:32 +0530

    added terraform config filed

--- main.tf
+++ main.tf
@@ -1,15 +1,17 @@
 module "module" {
 source        = "git::https://github.com/kaulaskar/sailesh_dev.git?ref=module"
-# vpc_cidr      = "10.0.0.0/16"
-# ami           = "ami-0f5ee92e2d63afc18"
-# instance_type = "t2.micro"
-# key_name      = "desktop-key"
-# subnet_cidr   = "10.0.0.0/24"
-# allow_all     = "0.0.0.0/0"
-# rt_cidr       = "0.0.0.0/0"
-# az            = "ap-south-1a"
-# server_name   = "server"
-# egress        = [22, 80]
-# ingress       = [22, 80, 443, 8080, 555]
-# region        = "ap-south-1"
- }
+#   source        = "./module"
+  vpc_cidr      = "10.0.0.0/16"
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = "t2.micro"
+  key_name      = "desktop-key"
+  subnet_cidr   = "10.0.0.0/24"
+  allow_all     = "0.0.0.0/0"
+  rt_cidr       = "0.0.0.0/0"
+  az            = "ap-south-1a"
+  server_name   = "server"
+  egress        = [22, 80]
+  ingress       = [22, 8080, 80, 443, 555]
+  region        = "ap-south-1"
+
+}
