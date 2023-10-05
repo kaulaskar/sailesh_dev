9b163112a88e466f541ac5f0b238fa8c78662975
kaulaskar <saileshkaulaskar@gmail.com>
2023-10-05 12:49:47 +0530

    modified the module file

--- variable.tf
+++ variable.tf
@@ -48,27 +48,16 @@
 
 
 
-variable "vpc_cidr" {}
-
-variable "ami" {}
-
-variable "instance_type" {}
-
-variable "key_name" {}
-
-variable "subnet_cidr" {}
-
-variable "allow_all" {}
-
-variable "rt_cidr" {}
-
-variable "az" {}
-
-variable "server_name" {}
-
-variable "egress" {}
-
-variable "ingress" {}
-
-variable "region" {}
+variable "vpc_cidr"{}
+variable "ami"{}
+variable "instance_type"{}
+variable "key_name"{}
+variable "subnet_cidr"{}
+variable "allow_all"{}
+variable "rt_cidr"{}
+variable "az"{}
+variable "server_name"{}
+variable "egress"{}
+variable "ingress"{}
+variable "region"{}
 
