412a61526a36f74abdcd88748deb43708301c22d
kaulaskar <saileshkaulaskar@gmail.com>
2023-10-05 09:17:55 +0530

    added files

--- init.tf
+++ init.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "devopsremotestate1"
-    key    = "dev/terraform.tfstate"
+    key    = "dev/module.tfstate"
     region = "us-east-1"
   }
 }
--- module/internetgatway.tf
+++
@@ -1,7 +0,0 @@
-######################IGW###############
-resource "aws_internet_gateway" "IGW" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "IGW"
-  }
-}
